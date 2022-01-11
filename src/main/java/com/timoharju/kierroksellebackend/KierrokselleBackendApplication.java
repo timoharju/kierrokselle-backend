01e7e7a7a8b29f507f25a4f90033f3163a918165
timoharju <timoharju17@gmail.com>
2022-01-11 17:46:49 +0200

    Spring init

---
+++ src/main/java/com/timoharju/kierroksellebackend/KierrokselleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.timoharju.kierroksellebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KierrokselleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KierrokselleBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/timoharju/kierroksellebackend/KierrokselleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timoharju.kierroksellebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KierrokselleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
